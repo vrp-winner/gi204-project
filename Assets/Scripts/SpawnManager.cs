d39dd538ddc66a4a9022e6043071363055695e10
vrp-winner <vorapol.saen@bumail.net>
2025-03-18 00:17:35 +0700

    Add script Spawn Manager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject[] obstaclePrefab;
+    
+    public float startDelay = 0.1f;
+    public float repeatLate = 1f;
+    
+    private PlayerController playerController; // อ้างอิงถึง PlayerController เพื่อตรวจสอบว่าเกมจบหรือยัง
+    void Start()
+    {
+        // ค้นหา "Car" PlayerController มาใช้
+        playerController = GameObject.Find("Car").GetComponent<PlayerController>();
+        
+        InvokeRepeating("Spawn", startDelay, repeatLate);
+    }
+
+    void Spawn()
+    {
+        int randomIndex = Random.Range(0, obstaclePrefab.Length);
+        Instantiate(obstaclePrefab[randomIndex], spawnPoint.position, obstaclePrefab[randomIndex].transform.rotation);
+    }
+}
